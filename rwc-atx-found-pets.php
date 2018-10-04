c461858e15a91b0ca0be63d9c0e60f0537ffb85c
Amber Hinds <amber@roadwarriorcreative.com>
2018-10-04 15:49:19 -0500

    creating the plugins

---
+++ rwc-atx-found-pets.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link              https://roadwarriorcreative.com
+ * @since             1.0.0
+ * @package           rwc_atx_found_pets
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Austin TX Found Pets Shortcode
+ * Plugin URI:        https://roadwarriorcreative.com
+ * Description:       This plugin creates a shortcode that displays all stray cats and dogs that are currently listed in Austin Animal Center's database. API integration example + help missing dogs and cats find their owners = good karma.  
+ * Version:           1.0.0
+ * Author:            Road Warrior Creative
+ * Author URI:        https://roadwarriorcreative.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       rwc-atx-found-pets
+ * Domain Path:       /languages
+ */
+
